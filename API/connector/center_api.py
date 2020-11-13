9e0f8c816ae7acc1ee30bdb8c4f4c6abaf1bb34a
Rhange <rhange0511@gmail.com>
2020-11-13 11:41:24 +0900

    API venv 삭제 및 조금 변경

--- API/connector/center_api.py
+++ API/connector/center_api.py
@@ -9,12 +9,16 @@ import os
 app = Flask(__name__)  # 'app'이라는 이름의 Flask Application 객체를 생성한다.
 
 
-@app.route("/")   # IP+포트번호만을 가지고 접속했을 때
+@app.route("/", methods=['GET', 'POST'])   # IP+포트번호만을 가지고 접속했을 때
 def home():
-    return render_template('index.html')  # 'index.html'의 내용을 참조하여 Page를 구성해 출력한다.
+    if request:
+        print(request)
+    # 'index.html'의 내용을 참조하여 Page를 구성해 출력한다.
+    return render_template('index.html')
 
 
-@app.route("/let_me_shine", methods=['GET', 'POST'])  # 첫 화면에서 Image 파일을 제출하고 나면, 본 Url Page로 접속하게 된다.
+# 첫 화면에서 Image 파일을 제출하고 나면, 본 Url Page로 접속하게 된다.
+@app.route("/let_me_shine", methods=['GET', 'POST'])
 def let_me_shine():
     time_flag = time.strftime('%m%d-%H%M%S', time.localtime(time.time()))
     # 메소드 호출(= 접속) 당시의 시간 정보를 '월일-시분초' 형식으로 기억해 둔다. 이는 파일 이름의 중복 방지에 활용될 것이다.
@@ -23,12 +27,17 @@ def let_me_shine():
 
     if request.method == 'POST':   # POST 방식으로 Request가 전달된 경우에의 동작이다.
         f = request.files['raw_img_file']   # Client가 첨부한 파일을 변수(객체)로 가져온다.
-        file_name = r'../image_2_style_gan/img/{}'.format(f.filename)  # 첨부한 Image가 업로드한 파일명과 형식 그대로 일단 저장될 위치를 지정한다.
-        client_img_name = '../image_2_style_gan/img/{}_{}.png'.format(client_ip, time_flag)  # 첨부한 Image가 png 형식으로 다시 저장될 경로와 이름을 지정한다.
+        # 첨부한 Image가 업로드한 파일명과 형식 그대로 일단 저장될 위치를 지정한다.
+        file_name = r'../image_2_style_gan/img/{}'.format(f.filename)
+        # 첨부한 Image가 png 형식으로 다시 저장될 경로와 이름을 지정한다.
+        client_img_name = '../image_2_style_gan/img/{}_{}.png'.format(
+            client_ip, time_flag)
         f.save(file_name)  # 변수에 받아들여 놓은 Image를 파일로 저장한다.
 
-        cnv_buffer = cv2.imread(file_name)  # 앞서 저장한 업로드 Image를 openCV Library의 'imread'메소드를 이용해 다시 읽어들인다.
-        cv2.imwrite(client_img_name, cnv_buffer)  # 접속한 Client의 IP를 활용해 지정한 이름으로, 읽어들였던 Image를 png파일로 다시 기록한다.
+        # 앞서 저장한 업로드 Image를 openCV Library의 'imread'메소드를 이용해 다시 읽어들인다.
+        cnv_buffer = cv2.imread(file_name)
+        # 접속한 Client의 IP를 활용해 지정한 이름으로, 읽어들였던 Image를 png파일로 다시 기록한다.
+        cv2.imwrite(client_img_name, cnv_buffer)
         os.remove(file_name)  # 기존의 원본 Image 파일은 삭제한다.
 
     input_image, output_image = image_crossover(client_ip, time_flag)
@@ -48,5 +57,6 @@ if __name__ == "__main__":
     # 즉, 이는 특정 Module을 타 Module에서 Import를 통해 활용하는 경우와 구분지을 수 있는 수단이 된다.
 
     print("Server Start")  # 메시지를 출력해 Server의 작동 시작을 알린다.
-    app.run('121.138.83.1', port=45045, debug=True)  # 생성한 'app' 객체를 Parameter 값들을 이용해 구동한다.
+    # 생성한 'app' 객체를 Parameter 값들을 이용해 구동한다.
+    app.run('121.138.83.1', port=45045, debug=True)
     # 위에서 활용된 Parameter는 IP(v4)와 포트 번호, 디버그 모드의 수행 여부에 대한 Boolean 값이다.
