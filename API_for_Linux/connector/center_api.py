30747be8bab85dad08b689681e8059ae587f401a
Rhange <rhange@localhost.localdomain>
2020-11-30 13:36:20 +0900

    첫번째 이미지 key 값 'text' -> 'origin' 반영

--- API_for_Linux/connector/center_api.py
+++ API_for_Linux/connector/center_api.py
@@ -29,7 +29,7 @@ def data_return():
 
 @app.route("/let_me_shine", methods=['GET', 'POST'])  # 첫 화면에서 Image 파일을 제출하고 나면, 본 Url Page로 접속하게 된다. (Web)
 def let_me_shine():
-    url_base = "http://222.106.22.97:45045/let_me_shine/results/?uid="
+    url_base = "http://222.106.22.97:55055/let_me_shine/results/?uid="
     # client_ip = request.remote_addr
     # 접속해 온 Client의 IP(v4) 주소를 기억해 둔다. 파일 이름 중복 혹은 혼선을 방지하기 위해 활용될 것이다.
     # time_flag = time.strftime('%m%d-%H%M%S', time.localtime(time.time()))
@@ -52,17 +52,17 @@ def let_me_shine():
 
     try:
         data = request.get_json(silent=True)
-        if not data['text']:
+        if not data['origin']:
             print("Re-send image, please.")
             return "Re-send image, please."
         else:
-            # item = {'label': data.get('label'), 'text': data.get('text')}
+            # item = {'label': data.get('label'), 'origin': data.get('origin')}
             file_name = f'../image_2_style_gan/images/raw/raw_{rand_uuid}.jpg'  # 첨부한 Image가 업로드한 파일명과 형식 그대로 일단 저장될 위치를 지정한다.
             client_img_name = f'../image_2_style_gan/images/raw/{rand_uuid}.png' # 첨부한 Image가 png 형식으로 다시 저장될 경로와 이름을 지정한다.
             # f.save(file_name)  # 변수에 받아들여 놓은 Image를 파일로 저장한다.
 
             with open(file_name, 'wb') as f:
-                f.write(base64.b64decode(data['text']))
+                f.write(base64.b64decode(data['origin']))
 
             cnv_buffer = cv2.imread(file_name)  # 앞서 저장한 업로드 Image를 openCV Library의 'imread'메소드를 이용해 다시 읽어들인다.
             cv2.imwrite(client_img_name, cnv_buffer)  # 접속한 Client의 IP를 활용해 지정한 이름으로, 읽어들였던 Image를 png파일로 다시 기록한다.
@@ -98,5 +98,5 @@ if __name__ == "__main__":
     # 즉, 이는 특정 Module을 타 Module에서 Import를 통해 활용하는 경우와 구분지을 수 있는 수단이 된다.
 
     print("Server Start!!")  # 메시지를 출력해 Server의 작동 시작을 알린다.
-    app.run('222.106.22.97', port=45045, debug=True)  # 생성한 'app' 객체를 Parameter 값들을 이용해 구동한다.
+    app.run('222.106.22.97', port=55055, debug=True)  # 생성한 'app' 객체를 Parameter 값들을 이용해 구동한다.
     # 위에서 활용된 Parameter는 IP(v4)와 포트 번호, 디버그 모드의 수행 여부에 대한 Boolean 값이다.
