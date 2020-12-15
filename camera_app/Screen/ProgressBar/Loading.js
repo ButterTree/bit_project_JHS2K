d0d40e47cd07f1beb64601041d03f995fa705c71
Rhange <rhange@localhost.localdomain>
2020-12-15 16:37:58 +0900

    asset 필요없는 파일들 삭제

--- camera_app/Screen/ProgressBar/Loading.js
+++ camera_app/Screen/ProgressBar/Loading.js
@@ -4,18 +4,26 @@ import { Image, View, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 export default function Loading() {
-  const [isReady, setIsReady] = useState(true);
+    const [isReady, setIsReady] = useState(true);
 
-
-  return isReady &&
-
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
-      <Image style={{
-        height: width,
-        width: width,
-        resizeMode: "contain"
-      }}
-        source={require('../../assets/images/Loading.gif')}
-      />
-    </View>
+    return (
+        isReady && (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Image
+                    style={{
+                        height: width,
+                        width: width,
+                        resizeMode: 'contain'
+                    }}
+                    source={require('../../assets/Loading.gif')}
+                />
+            </View>
+        )
+    );
 }
