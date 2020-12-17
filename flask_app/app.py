9ded44bc706c4905d63002231ee7eee196319c96
Rhange <rhange@localhost.localdomain>
2020-12-17 19:56:21 +0900

    nginx gunicorn flask  연   연 post 연  연   연

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,3 +1,4 @@
+import json
 from connector.gpu_task.model_task import main_processing
 from flask import Flask, request
 
@@ -5,9 +6,12 @@ import uuid
 
 server = Flask(__name__)
 
-@server.route("/", methods=['POST'])
+@server.route("/", methods=['GET', 'POST'])
 def let_me_shine():
-    rand_uuid = uuid.uuid4()
-    data = request.get_json(silent=True)
-    json_data = main_processing(data, rand_uuid)
-    return json_data
+    if request.method == 'POST':
+        rand_uuid = uuid.uuid4()
+        data = request.get_json()
+        json_data = main_processing(data, rand_uuid)
+        return json_data
+    else:
+        return "Hi"
--- flask_app/wsgi.py
+++ flask_app/wsgi.py
@@ -2,4 +2,3 @@ from app import server
 
 if __name__ == "__main__":
     server.run(host='192.168.1.47', port=8000)
-
