455dfa2cbb1c1cb4f5b68d280f9561713e1d9836
Rhange <rhange@localhost.localdomain>
2020-12-18 16:40:43 +0900

    container 3 try

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,16 +1,10 @@
-from .connector.gpu_task.model_task import main_processing
 from flask import Flask, request
-
-import uuid
 
 server = Flask(__name__)
 
 @server.route("/", methods=['GET', 'POST'])
 def let_me_shine():
     if request.method == 'POST':
-        rand_uuid = uuid.uuid4()
-        data = request.get_json()
-        json_data = main_processing(data, rand_uuid)
-        return data
+        return "Post"
     else:
-        return "Hi"
+        return "Get"
--- flask_app/wsgi.py
+++ flask_app/wsgi.py
@@ -1,4 +1,4 @@
 from app import server
 
 if __name__ == "__main__":
-    server.run(host='192.168.1.47', port=8000)
+    server.run(host='192.168.1.47', port=8000, debug=True)
