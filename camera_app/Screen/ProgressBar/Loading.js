c5851f97175a9cb8ba91ffae6339d2d4b98e05ac
Rhange <rhange@localhost.localdomain>
2020-12-21 14:07:00 +0900

    For test, add camera_app

---
+++ camera_app/Screen/ProgressBar/Loading.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Image, View, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+export default function Loading() {
+    const [isReady, setIsReady] = useState(true);
+
+    return (
+        isReady && (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Image
+                    style={{
+                        height: height / 2,
+                        width: width / 1
+                    }}
+                    source={require('../../assets/Loading.gif')}
+                />
+            </View>
+        )
+    );
+}
--- flask_app/wsgi.py
+++
@@ -1,4 +0,0 @@
-from app import server
-
-if __name__ == "__main__":
-    server.run(host='192.168.1.47', port=8000, debug=True)
