7bbaf5359d100954c81b746c882e6e0456a25b41
Rhange <rhange@localhost.localdomain>
2020-12-22 11:15:05 +0900

    Cleaning

--- camera_app/components/Buttons/PopupBtns/TwoPeopleLights/TwoPeopleLightsContainer.js
+++
@@ -1,9 +0,0 @@
-export const useLightState = () => {
-  return {
-    firstLightColor: '#FADBDB',
-    firstLightText: '1st',
-    secondLightColor: '#FADBDB',
-    secondLightText: '2nd',
-    LightDefaultColor: 'transparent',
-  };
-};
