301d8c2cf6af5277ed1d600511b85d1d29710c37
JunWorld <65802905+ComeToJun@users.noreply.github.com>
2021-01-01 13:11:51 +0900

    update 0101
    app.json update:false -> true
    Text.js -> 쌍토끼:Pink

--- camera_app/API/api.js
+++ camera_app/API/api.js
@@ -2,9 +2,9 @@
 
 import axios from 'axios';
 import { Alert } from 'react-native';
-import { SERVER_IP } from './apiAddress';
+import { SERVER_IP, SERVER_PORT } from './apiAddress';
 
-const URL = `http://${SERVER_IP}`;
+const URL = `http://${SERVER_IP}:${SERVER_PORT}`;
 
 let TEMP_RESULT = [];
 
